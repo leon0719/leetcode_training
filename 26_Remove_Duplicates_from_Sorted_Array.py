61e844ecd7c099fe920aedfec2237b699b5855a7
leon0719 <leon0970865551@gmail.com>
2023-01-07 22:50:31 +0800

    1次

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -1,17 +1,18 @@
 from typing import List
+import pysnooper
 #題目輸入一個由小到大排序的陣列，
 #我們需要回傳『沒有重複元素陣列長度』
 #並將這些沒有重複的元素移動至該陣列的開頭。
-
+@pysnooper.snoop()
 def removeDuplicates(nums: List[int]) -> int:
-    i = 0
-    while i < len(nums)-1:
-        if nums[i] == nums[i+1]:
-            nums.remove(nums[i])
-            i -= 1
-        i += 1
+    index = 0
+    for i in range(len(nums)):
+        if (i == 0 or nums[i] != nums[i-1]):
+            nums[index] = nums[i]
+            index += 1
 
-    return len(nums)
+    return index
+
 
 if __name__ == "__main__":
     test1 = [0,0,1,1,1,2,2,3,3,4]
