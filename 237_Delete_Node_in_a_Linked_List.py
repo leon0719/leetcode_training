b3b2d18963883d6d09475551e9f2c490c4ae6988
leon0719 <leon0970865551@gmail.com>
2023-01-10 12:22:33 +0800

    1次

---
+++ 237_Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,10 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        node.next,node.val = node.next.next ,node.next.val
+
+#只能在leetcode跑
