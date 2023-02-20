9ba54431a34e491026ee65f8a044d9512de247d0
leon0719 <leon0970865551@gmail.com>
2023-02-20 11:21:32 +0800

    update

---
+++ 217_Contains_Duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+def containsDuplicate(nums: List[int]) -> bool:
+    set_list = set(nums)
+    if len(nums) != len(set_list):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    a = containsDuplicate([1,2,3,1])
+    print(a)
---
+++ 70_Climbing_Stairs.py
@@ -0,0 +1,19 @@
+
+def climbStairs(n: int) -> int:
+    if n == 1 :
+        return 1
+    elif n == 2 :
+        return 2
+    result = 0
+    pre = 1
+    next = 2
+    for _ in range(2,n):
+        result = pre +next
+        pre = next
+        next = result
+    return result
+
+
+if __name__ == '__main__':
+    out =climbStairs(4)
+    print(out)
