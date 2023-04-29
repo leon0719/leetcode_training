575e108fe1d719c44841b8b9bce9cd7734ecf613
leon0719 <leon0970865551@gmail.com>
2023-04-29 14:18:27 +0800

    update

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,19 +1,26 @@
 from typing import List
 
-# def twoSum(nums: List[int], target: int) -> List[int]:
-#     hashtable = {}
-#     for i in range(len(nums)):
-#         complement = target - nums[i]
-#         if complement not in hashtable:
-#             hashtable[nums[i]] = i
-#         else:
-#             print(hashtable)
-#             return [hashtable[complement], i]
-#     return None
+def twoSum(nums: List[int], target: int) -> List[int]:
+    hashmap = {}
+    for i in range(len(nums)):
+        temp = target - nums[i]
+        if temp not in hashmap.keys():
+            hashmap[nums[i]] = i
+        else :
+            return [hashmap[temp], i]
 
 
-
-if __name__ == "__main__":
-    print(twoSum(test1,target1))
-
-
+def test_twoSum():
+    # unit test twoSum
+    nums = [2, 7, 11, 15]
+    target = 9
+    assert (twoSum(nums, target) == [0, 1])
+    print('Pass')
+    print('---------------------')
+def test_twoSum2():
+    # unit test twoSum
+    nums = [3, 2, 4]
+    target = 6
+    assert (twoSum(nums, target) == [1, 2])
+    print('Pass')
+    print('---------------------')
