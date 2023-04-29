fb958bc105033e988e50c299920a38ee0e21e49c
leon0719 <leon0970865551@gmail.com>
2023-04-29 13:46:55 +0800

    update

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,22 +1,15 @@
 from typing import List
 
-test1 = [2,7,11,15,4,5]
-target1 = 7
-test2 = [3,2,4,7,8]
-target2 = 15
-test3 = [3,3]
-target3 = 6
-
-def twoSum(nums: List[int], target: int) -> List[int]:
-    hashtable = {}
-    for i in range(len(nums)):
-        complement = target - nums[i]
-        if complement not in hashtable:
-            hashtable[nums[i]] = i
-        else:
-            print(hashtable)
-            return [hashtable[complement], i]
-    return None
+# def twoSum(nums: List[int], target: int) -> List[int]:
+#     hashtable = {}
+#     for i in range(len(nums)):
+#         complement = target - nums[i]
+#         if complement not in hashtable:
+#             hashtable[nums[i]] = i
+#         else:
+#             print(hashtable)
+#             return [hashtable[complement], i]
+#     return None
 
 
 
