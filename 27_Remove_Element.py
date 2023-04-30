c3f09e522a01778fb0f13e7f4407390733c5b158
leon0719 <leon0970865551@gmail.com>
2023-04-30 21:19:29 +0800

    update

--- 27_Remove_Element.py
+++ 27_Remove_Element.py
@@ -1,18 +1,18 @@
 from typing import List
 def removeElement(nums: List[int], val: int) -> int:
-    if len(nums) == 0:
-        return len(nums)
-    elif val not in nums:
-        return len(nums)
-    count = 0
-    for i in range(len(nums)):
-        if nums[i] != val:
-            nums[count] = nums[i]
-            count += 1
-    return count
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != val:
+            nums[i] = nums[j]
+            i += 1
+    return i
 
 
-if __name__ == "__main__":
-    test1=[3,2,2,3]
-    val1 = 3
-    print(removeElement(test1,val1))
+# unit test removeElement
+
+def test_removeElement():
+    nums = [3,2,2,3]
+    val = 3
+    assert (removeElement(nums, val) == 2)
+    
+
