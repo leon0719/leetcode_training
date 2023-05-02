10508aaeb762367c5a2e981ef792b7085fbfc7c7
leon0719 <leon0970865551@gmail.com>
2023-05-02 16:47:05 +0800

    update

--- 66_Plus_One.py
+++ 66_Plus_One.py
@@ -1,12 +1,23 @@
 from typing import List
 
 def plusOne(digits: List[int]) -> List[int]:
-    return [int(i) for i in str(int(''.join(str(dig) for dig in digits))+1)]
+    return [int(i) for i in str(int(''.join([str(i) for i in digits]))+1)]
 
 
 
+    # return [int(i) for i in str(int(''.join(str(dig) for dig in digits))+1)]
 
-if __name__ == "__main__":
-    test =[9]
-    print(plusOne(test))
 
+# unit test plusOne
+
+def test_plusOne():
+    assert (plusOne([1, 2, 3]) == [1, 2, 4])
+    assert (plusOne([4, 3, 2, 1]) == [4, 3, 2, 2])
+    assert (plusOne([0]) == [1])
+    assert (plusOne([9]) == [1, 0])
+    assert (plusOne([9, 9]) == [1, 0, 0])
+    assert (plusOne([9, 9, 9]) == [1, 0, 0, 0])
+    assert (plusOne([9, 9, 9, 9]) == [1, 0, 0, 0, 0])
+    assert (plusOne([9, 9, 9, 9, 9]) == [1, 0, 0, 0, 0, 0])
+    assert (plusOne([9, 9, 9, 9, 9, 9]) == [1, 0, 0, 0, 0, 0, 0])
+    assert (plusOne([9, 9, 9, 9, 9, 9, 9]) == [1, 0, 0, 0, 0, 0, 0, 0])
