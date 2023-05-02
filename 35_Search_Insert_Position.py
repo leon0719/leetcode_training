293b6d3ffd81a195708b9b811a4a9a77596c452e
leon0719 <leon0970865551@gmail.com>
2023-05-02 15:05:54 +0800

    update

--- 35_Search_Insert_Position.py
+++ 35_Search_Insert_Position.py
@@ -1,19 +1,23 @@
 from typing import List
 def searchInsert(nums: List[int], target: int) -> int:
-    left,right = 0,len(nums)-1
+    left , right = 0 , len(nums)- 1
     while left <= right:
-        mid = (left + right) //2
-        if nums[mid] > target:
-            right = mid - 1
-        elif nums[mid] < target:
-            left = mid + 1
-        else:
+        mid = (left + right) // 2
+        if nums[mid] == target :
             return mid
+        elif nums[mid] > target :
+            right = mid -1
+        else:
+            left = mid +1
     return left
 
 
-
-if __name__ == "__main__":
-    test1 = [1,3,5,6]
-    target = 7
-    print(searchInsert(test1, target))
+def test_searchInsert():
+    assert (searchInsert([1, 3, 5, 6], 5) == 2)
+    print('All tests passed!')
+def test_searchInsert2():
+    assert (searchInsert([1, 3, 5, 6], 2) == 1)
+    print('All tests passed!')
+def test_searchInsert3():
+    assert (searchInsert([1, 3, 5, 6], 7) == 4)
+    print('All tests passed!')
